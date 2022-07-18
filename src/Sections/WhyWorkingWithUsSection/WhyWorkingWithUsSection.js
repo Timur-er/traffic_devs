d86f80b8d5404503d63e8c2bfe8d6aa475095d5d
Timur-er <etimur808@gmail.com>
2022-07-18 22:22:00 +0300

    change text

--- src/Sections/WhyWorkingWithUsSection/WhyWorkingWithUsSection.js
+++ src/Sections/WhyWorkingWithUsSection/WhyWorkingWithUsSection.js
@@ -13,8 +13,7 @@ const WhyWorkingWithUsSection = () => {
                     <div className={styles.inventory__content}>
                         <div className={styles.inventory__content_section}>
                             <p>
-                                Traffic DevS is DSP built to deliver results. Powered by a unique AI algorithm, RTBsystem predicts, in real time, all possible outcomes from every single user available within the programmatic ecosystem, so the results are almost guaranteed.
-                            </p>
+                                In Traffic DevS we always want to make sure that our clients get the best results. Therefore we use various systems to improve targeting and promote products only on the most relevant and verified websites and other sources.                            </p>
                         </div>
                         <div className={styles.inventory__svg_wrapper}>
                             <FirstInventorySVG  className={styles.inventory__svg}/>
@@ -23,9 +22,7 @@ const WhyWorkingWithUsSection = () => {
                     <div className={styles.inventory__content}>
                         <div className={styles.inventory__svg_wrapper}><SecondInventorySVG  className={styles.inventory__svg}/></div>
                         <div className={styles.inventory__content_section}>
-                            <p>In Traffic DevS we always want to make sure that our clients get the best results. Therefore we use various systems to improve targeting and promote products only on the most relevant and verified websites and other sources.
-
-                                We use various targeting types like Browser, Keywords, OS and device, Carrier, Interests, Connection types etc. to only attract people who will like your product.</p>
+                            <p>We use various targeting types like Browser, Keywords, OS and device, Carrier, Interests, Connection types etc. to only attract people who will like your product.</p>
                         </div>
                     </div>
                 </div>
